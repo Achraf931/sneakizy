9ffc29ad7dcf5240b54765ab9d46da3af9853125
Achraf <hamrouni.pro@outlook.fr>
2020-04-28 18:26:38 +0200

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ImageTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
         $this->call(NewsTableSeeder::class);
-        //$this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(BrandsTableSeeder::class);
     }
 }
