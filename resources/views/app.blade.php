dff9e55ed5a23a59b8b0bb5b12b5f441c8d8a0d3
Achraf <hamrouni.pro@outlook.fr>
2020-04-28 19:03:42 +0200

    test property og

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta property="og:site_name" content = "SNKEAKiZY"/>
+        <meta property="og:title" content="SNKEAKiZY"/>
+        <meta property="og:url" content="https://sneakizy.herokuapp.com/"/>
+        <meta property="og:image" content="https://cdn.shopify.com/s/files/1/0025/2687/1622/files/logo_nike_1024x1024.jpg?v=1572383751"/>
+        <meta property="og:description" content="Boutique en ligne de sneakers."/>
         <title>Sneakizy</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
